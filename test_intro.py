04745e6c1333192160511aebe7ab3bc0b25a2222
Erik Carlson <erikvcarlson@gmail.com>
2024-08-08 18:11:58 -0400

    rewriting test funcs

--- test_intro.py
+++ test_intro.py
@@ -1,11 +1,82 @@
+from WingWatch.Deprecated import tritrioverlap
 from WingWatch.Equipment import station
 from WingWatch.Equipment import antenna
 from WingWatch.Intersections import montecarlo
-from WingWatch.Tools import translation,rotation, tritrioverlap
+from WingWatch.Tools import translation,rotation,point_check
 import pandas as pd
 import numpy as np
+import scipy.spatial as ss
 
 
+
+
+
+def points_on_unit_sphere(theta,phi,offset_x = 0,offset_y=0,offset_z=0):
+    x = np.sin(theta) * np.cos(phi) + offset_x
+    y = np.sin(theta) * np.sin(phi) + offset_y
+    z = np.cos(theta)  +offset_z
+    return np.array([x,y,z])
+
+
+def test_points_on_unit_sphere():
+    #Function to Test: Tools.point_check.point_in_hull
+    #given a test point at (0,0,0) does exist in a sphere of radius 1
+    expected =True
+
+    
+    offset_x = 0
+    offset_y = 0 
+    offset_z = 0
+  
+    theta = np.linspace(-np.pi,np.pi,30)
+    phi = np.linspace(-2*np.pi,2*np.pi,30)
+
+    points = []
+
+    for i in phi:
+        for j in theta:       
+            points.append(points_on_unit_sphere(i,j))
+
+    points = np.row_stack(np.array(points))
+    
+    #we can show that every point exists on the unit sphere by showing its radius from the origin is 1. 
+    #note that python's floating point precision is only good to 16 digits, this we need to round to 15 digits. 
+
+    radius_of_point = np.round(np.sqrt(np.sum([np.power(points[:,0],2),np.power(points[:,1],2),np.power(points[:,2],2)],axis=0)),15)
+
+    test_sol = np.all(radius_of_point == 1)
+
+    assert expected == test_sol 
+
+
+def test_point_in_center_of_sphere():
+    #Function to Test: Tools.point_check.point_in_hull
+    #given a test point at (0,0,0) does exist in a sphere of radius 1
+    
+    expected =True
+
+    test_point = np.array([0,0,0])
+    
+    theta = np.linspace(-np.pi,np.pi,30)
+    phi = np.linspace(-2*np.pi,2*np.pi,30)
+
+    points = []
+
+    for i in phi:
+        for j in theta:       
+            points.append(points_on_unit_sphere(i,j))
+
+
+
+    tess_sphere = ss.ConvexHull(points)
+
+    test_sol = point_check.point_in_hull(test_point,tess_sphere)
+
+    assert expected == test_sol 
+
+
+
+'''
 def test_motecarlo_intersection_single_station():    
     BRR_Station = station.Station('BRR',41.1479, -71.5901)
     BRR_Comp_Data = pd.read_csv('tests/Fake_Calibration_Data.csv')
@@ -106,3 +177,5 @@ def test_triangle_intersection_intersect():
     expected = 1
     test_sol = tritrioverlap.triTriOverlapTest3d(V10, V11, V12, V20, V21, V22)
     assert expected == test_sol
+
+'''
