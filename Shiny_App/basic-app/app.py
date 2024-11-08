c421619ca03082bf5a6581f6ad788c1b9546af7b
Erik Carlson <erikvcarlson@gmail.com>
2024-11-08 10:00:09 -0500

    User and Generating Detections

--- Shiny_App/basic-app/app.py
+++ Shiny_App/basic-app/app.py
@@ -1,4 +1,4 @@
-from ipyleaflet import Map, basemaps
+from ipyleaflet import Map, basemaps, Marker, Circle
 from ipywidgets import Layout  
 from shiny import App, reactive, ui
 from shinywidgets import output_widget, render_widget  
@@ -6,6 +6,8 @@ from WingWatch.Equipment import station, antenna
 from WingWatch.Intersections import tri
 from WingWatch.Tools import point_check as pc
 from WingWatch.Intersections.detection import Detection
+from WingWatch.Tools import translation
+import WingWatch.Tools.spheres as sph
 import pandas as pd
 import os
 import time
@@ -14,6 +16,7 @@ import logging
 import numpy as np
 import pickle
 import shutil
+import scipy.spatial as ss
 
 # Logging setup
 logger = logging.getLogger(__name__)
@@ -29,7 +32,17 @@ app_ui = ui.page_sidebar(
         bg="#f8f8f8",    
         ),
     ui.layout_columns(
-        ui.card("Data Input"),
+            ui.card(
+            ui.card_header("Station Creation Terminal"),
+            ui.input_text("station_name", "Name of Station", "Station Name"),
+            ui.input_numeric("lat_val_stat", "Latitude", 41, min=-90, max=90),
+            ui.input_numeric("long_val_stat", "Longitude", -71, min=-180, max=180),
+            ui.input_task_button("generate_station", "Generate"),
+            ui.input_select("select_stat", "Select a Station", choices=list()),
+            ui.input_numeric("ant_name", "Antenna Number", 1, min=1),
+            ui.input_file("pattern_entry", "CSV for Pattern", accept='.csv'),
+            ui.input_task_button("Assign_Pattern", "Assign Pattern")
+        ),
         ui.card(ui.card_header("Map"), output_widget("map")),
         ui.card(
             ui.card_header("Detection Generator"),
@@ -46,17 +59,7 @@ app_ui = ui.page_sidebar(
         ),
     ),
     ui.layout_columns(
-        ui.card(
-            ui.card_header("Station Creation Terminal"),
-            ui.input_text("station_name", "Name of Station", "Station Name"),
-            ui.input_numeric("lat_val_stat", "Latitude", 41, min=-90, max=90),
-            ui.input_numeric("long_val_stat", "Longitude", -71, min=-180, max=180),
-            ui.input_task_button("generate_station", "Generate"),
-            ui.input_select("select_stat", "Select a Station", choices=list()),
-            ui.input_numeric("ant_name", "Antenna Number", 1, min=1),
-            ui.input_file("pattern_entry", "CSV for Pattern", accept='.csv'),
-            ui.input_task_button("Assign_Pattern", "Assign Pattern")
-        ),
+        ui.card("Data Input"),
         ui.card("Map Properties and Layer Control"),
         ui.card("Tesserlation Visualizer")
     ),
@@ -64,18 +67,29 @@ app_ui = ui.page_sidebar(
 
 # Shiny app server logic
 def server(input,output,session):
-    user_working_dir = reactive.Value('/tmp/users/')
+    
+    user_working_dir = reactive.value()
 
-    # Function to handle user button click (user_select)
-    @reactive.Effect
-    @reactive.event(input.user_select)  # React to button click
-    def update_user_directory():
-        # Get user input
-        user = input.user()
+    @reactive.effect
+    def startup_modal():
+        # Define the modal with a dismiss button
+        logger.info('Showed Modal Message')
+        ui.modal_show(
+            ui.modal(
+                ui.input_action_button("close_modal","Close"),
+                ui.p('You are using a random user. If you would like to use a known user, use the user selection tool on the right.'),
+                title="Welcome!",
+                easy_close=True,
+                footer=None
+            )
+        )
 
-        # If user is 1000, generate a random user ID between 10000 and 11000
-        if user == 1000:
-            user = np.random.randint(10000, 11000)
+    @reactive.effect
+    @reactive.event(input.close_modal)
+    def startup_fn():
+        ui.modal_remove()
+        # Code to execute on startup
+        user = np.random.randint(10000, 11000)
         
         new_dir = f'/tmp/users/{user}/'
         user_working_dir.set(new_dir)
@@ -85,47 +99,82 @@ def server(input,output,session):
             os.makedirs(user_working_dir.get())
             
         # Reactive output to provide choices for input_select based on user_working_dir
-        stations = glob.glob(user_working_dir.get() + '*.pkl')
+        stations_long = glob.glob(user_working_dir.get() + '*.pkl')
+        stations = [os.path.splitext(os.path.basename(file))[0] for file in stations_long]
+        time.sleep(1)
+        logger.info('Directory Function Ran on Start-up')
+        logger.info('Start-up Script Ran')
+        logger.info(f'The current user directory is: {user_working_dir.get()}')
+
+
+    # Function to handle user button click (user_select)
+    @reactive.effect
+    @reactive.event(input.user_select)  # React to button click
+    def update_user_directory():
+        # Get user input
+        user = input.user()
+       
+        new_dir = f'/tmp/users/{user}/'
+        user_working_dir.set(new_dir)
+        
+        # Create directory if it doesn't exist
+        if not os.path.exists(user_working_dir.get()):
+            os.makedirs(user_working_dir.get())
+            
+        # Reactive output to provide choices for input_select based on user_working_dir
+
+        file_list =  glob.glob(user_working_dir.get() +'*.pkl')
+        file_list_no_ext = [os.path.splitext(os.path.basename(file))[0] for file in file_list]
+        ui.update_select("select_stat", choices=file_list_no_ext)
+        ui.update_select("select_stat_det_1", choices=file_list_no_ext)
+        ui.update_select("select_stat_det_2", choices=file_list_no_ext)
+        ui.update_select("select_stat_det_3", choices=file_list_no_ext)
+
+
         time.sleep(1)
         logger.info('Directory Function Ran')
 
     @render_widget
     def map():
-        return Map(
+        m = Map(
             basemap=basemaps.Esri.WorldImagery,
             center=(41, -71),
             zoom=5,
             layout=Layout(width='500px', height='500px')
         )
 
-    @ui.bind_task_button(button_id="generate_station")
-    @reactive.extended_task
-    async def write_station_out(stationName, latValStat, longValStat):
-        logger.info('Intializing Station Function')
-        Station_1 = station.Station(stationName, latValStat, longValStat)
-        logger.info(f"Station Generated. Current Working Directory: {user_working_dir.get()}")
-        filename = user_working_dir.get() + f'{Station_1.name}.pkl'
-        save_user_data(Station_1, filename)
+        return m
 
+    #@ui.bind_task_button()
+    #@reactive.effect
+    #@reactive.event(input.generate_station)
     @reactive.effect
-    @reactive.event(input.generate_station, ignore_none=False)
+    @reactive.event(input.generate_station, ignore_none=False,ignore_init= True)
     def handle_click():
-        write_station_out(input.station_name(), float(input.lat_val_stat()), float(input.long_val_stat()))
+        logger.info('The Station Generation Button was Clicked.')
+        logger.info(f'The working directory for station generation is: {user_working_dir.get()}')
+
+        write_station_out(input.station_name(), float(input.lat_val_stat()), float(input.long_val_stat()),user_working_dir.get())
         time.sleep(1)
+        m = map.widget
+        point = Marker(location=(input.lat_val_stat(), input.long_val_stat()), draggable=False)     
+        m.add_layer(point)  
         file_list =  glob.glob(user_working_dir.get() +'*.pkl')
-        file_list_no_ext = [file[:-4] for file in file_list]
+        file_list_no_ext = [os.path.splitext(os.path.basename(file))[0] for file in file_list]
         ui.update_select("select_stat", choices=file_list_no_ext)
         ui.update_select("select_stat_det_1", choices=file_list_no_ext)
         ui.update_select("select_stat_det_2", choices=file_list_no_ext)
         ui.update_select("select_stat_det_3", choices=file_list_no_ext)
 
-    @ui.bind_task_button(button_id="Assign_Pattern")
-    @reactive.extended_task
-    async def update_station_class(name, ant_number, pattern):
+    @reactive.effect
+    @reactive.event(input.Assign_Pattern, ignore_none=False,ignore_init= True)
+    def handle_click_assign_pattern():
         try:
-            name = filename = user_working_dir.get() + f'{name}.pkl'
-            ant_number = int(ant_number)
-            pattern = pd.read_csv(pattern)
+            logger.info('Initialized the Assigning Pattern')
+            file = input.pattern_entry()[0]["datapath"]
+            name = user_working_dir.get() + f'{input.select_stat()}.pkl'
+            ant_number = int(input.ant_name())
+            pattern = pd.read_csv(file)
             Station_1 = load_object_from_disk(name)
             a1 = antenna.Antenna(ant_number, 'test', 0, 0)
             a1.assign_pattern(pattern)
@@ -135,35 +184,45 @@ def server(input,output,session):
         except Exception as err:
             logger.error(err)
 
-    @reactive.effect
-    @reactive.event(input.Assign_Pattern, ignore_none=False)
-    def handle_click():
-        try:
-            file = input.pattern_entry()[0]["datapath"]
-            update_station_class(input.select_stat(), input.ant_name(), file)
-            time.sleep(1)
-        except Exception as err:
-            logger.error(err)
+        time.sleep(1)
 
-    @ui.bind_task_button(button_id="Detect")
-    @reactive.extended_task
-    async def calc_intersect(select_stat_det_1_str, select_stat_det_2_str, select_stat_det_3_str,
-                             stat_1_strength_float, stat_2_strength_float, stat_3_strength_float,
-                             select_ant_det_1_int, select_ant_det_2_int, select_ant_det_3_int):
+
+    @reactive.effect
+    @reactive.event(input.Detect, ignore_none=False,ignore_init= True)
+    def calc_intersect():
         try:
             logger.info("Loading Stations")
-            Station_1 = load_object_from_disk(user_working_dir.get() + f'{select_stat_det_1_str}.pkl')
-            Station_2 = load_object_from_disk(user_working_dir.get() + f'{select_stat_det_2_str}.pkl')
-            Station_3 = load_object_from_disk(user_working_dir.get() + f'{select_stat_det_3_str}.pkl')
+            Station_1 = load_object_from_disk(user_working_dir.get() + f'{input.select_stat_det_1.get()}.pkl')
+            Station_2 = load_object_from_disk(user_working_dir.get() + f'{input.select_stat_det_2.get()}.pkl')
+            Station_3 = load_object_from_disk(user_working_dir.get() + f'{input.select_stat_det_3.get()}.pkl')
             logger.info("Stations Loaded Successfully")
 
-            det1 = Detection(Station_1, float(stat_1_strength_float), int(select_ant_det_1_int) - 1)
-            det2 = Detection(Station_2, float(stat_2_strength_float), int(select_ant_det_2_int) - 1)
-            det3 = Detection(Station_3, float(stat_3_strength_float), int(select_ant_det_3_int) - 1)
+            det1 = Detection(Station_1, float(input.stat_1_strength.get()), int(input.select_ant_det_1.get()) - 1)
+            det2 = Detection(Station_2, float(input.stat_2_strength.get()), int(input.select_ant_det_2.get()) - 1)
+            det3 = Detection(Station_3, float(input.stat_3_strength.get()), int(input.select_ant_det_3.get()) - 1)
 
             data_to_send_through = [det1, det2, det3]
             intersections, hull_of_intersections = tri.overlap_of_three_radiation_patterns(data_to_send_through)
             logger.info("Hull of Intersections Generated")
+
+            V = ss.ConvexHull(intersections)
+            radius = sph.find_radius_from_vol(V)
+
+            cx = np.mean(hull_of_intersections.points[hull_of_intersections.vertices,0])
+            cy = np.mean(hull_of_intersections.points[hull_of_intersections.vertices,1])
+            cz = np.mean(hull_of_intersections.points[hull_of_intersections.vertices,2])
+
+            Detection_pos = translation.convert_back_to_lla([cx,cy,cz],Station_1.lat,Station_1.long,Station_1.alt)
+
+
+            circle = Circle()
+            circle.location(Detection_pos[0],Detection_pos[1])
+            circle.radius(radius)
+            circle.color = "green"
+
+            m = map.widget
+            m.add(circle)
+            
             pc.point_in_hull(np.array([293, 211, 27]), hull_of_intersections)
             logger.info("Function Completed Successfully")
         except Exception as err:
@@ -191,5 +250,19 @@ def load_object_from_disk(file_path):
     """Load an object from disk using pickle."""
     with open(file_path, 'rb') as file:
         return pickle.load(file)
+
+def write_station_out(stationName, latValStat, longValStat,working_dir):
+        logger.info('Intializing Station Function')
+        logger.info(f'Station Parameters are;Station Name  = {stationName}, Lat = {latValStat}, Long = {longValStat}')
+        Station_1 = station.Station(stationName, latValStat, longValStat)
+        logger.info("Station Generated.")
+        try:
+            logger.info(f'Current Working Directory: {working_dir}')
+        except Exception as err:
+            logger.error(err)
+
+        filename = working_dir + f'{Station_1.name}.pkl'
+        save_object_to_disk(Station_1, filename)
+
 
 app = App(app_ui,server)
